276bbb614919597f1c8735fe825c67ddcd3508cb
Arif Shahriar Anik <anik001@gannon.edu>
2025-03-17 14:27:29 -0400

    Initial commit

---
+++ app/src/main/java/edu/gannon/todocontentproviderapplication/ToDoAdapter.java
@@ -0,0 +1,15 @@
+package edu.gannon.todocontentproviderapplication;
+
+import androidx.annotation.NonNull;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.TodoViewHolder> {
+    List<String>
+
+    @NonNull
+    @Override
+    Public TodoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        holder.taskText.setText(taskLis,get)
+    }
+If (TodoCursor != null)
+    
+}
